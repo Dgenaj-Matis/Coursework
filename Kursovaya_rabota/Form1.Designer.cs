96cd3a5edf4bf663096916b0f5d316e046288f46
Dgenaj Matis <mishin.mak-mi14@yandex.ru>
2020-10-01 23:10:23 +0300

    Добавьте файлы проекта.

---
+++ Kursovaya_rabota/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Kursovaya_rabota
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(61, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(901, 382);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1065, 417);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 82);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1052, 103);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1052, 151);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1052, 199);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1052, 247);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(172, 22);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1052, 295);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(172, 22);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(1052, 343);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(172, 22);
+            this.textBox6.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Thistle;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(1031, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 22);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Добавление данных:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Thistle;
+            this.label2.Location = new System.Drawing.Point(1061, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Номер помещения";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Thistle;
+            this.label3.Location = new System.Drawing.Point(1073, 132);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Вид животного";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Thistle;
+            this.label4.Location = new System.Drawing.Point(1100, 180);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Кличка";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Thistle;
+            this.label5.Location = new System.Drawing.Point(1100, 228);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Возраст";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Thistle;
+            this.label6.Location = new System.Drawing.Point(1107, 276);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Рацион";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Thistle;
+            this.label7.Location = new System.Drawing.Point(1032, 324);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(210, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Фамилия работника зоопарка";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Firebrick;
+            this.button2.Location = new System.Drawing.Point(61, 417);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 82);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.YellowGreen;
+            this.button3.Location = new System.Drawing.Point(217, 417);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 82);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(628, 417);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 82);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Вывод данных в Calc ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(809, 417);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(153, 82);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Вывод в файл .txt";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1382, 563);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Зоопарк";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Kursovaya_rabota/Form1.cs
@@ -0,0 +1,261 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using GemBox.Spreadsheet;
+using System.IO;
+using System.Diagnostics;
+
+namespace Kursovaya_rabota
+{
+    public partial class Form1 : Form
+
+    {
+        DataSet ds;
+        MySqlDataAdapter dataAdapter;
+        string ConnectString = "Database = zoo_animal2; Data Source = localhost; User Id = id; Password = pass";
+        string CommandText = "select * from zoo_animal2.animals";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.dataGridView1.BackgroundColor = Color.CornflowerBlue;
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;  //устанавливаем полное выделение строки 
+            dataGridView1.AllowUserToAddRows = false;   //запрет на ручное добавление строк
+            
+            button5.Enabled = false;
+
+
+            using (MySqlConnection connection = new MySqlConnection(ConnectString))
+            {
+                connection.Open();
+                dataAdapter = new MySqlDataAdapter(CommandText, connection);
+
+                ds = new DataSet();
+
+                MySqlCommandBuilder bulder = new MySqlCommandBuilder(dataAdapter);
+                dataAdapter.UpdateCommand = bulder.GetUpdateCommand();
+                dataAdapter.InsertCommand = bulder.GetInsertCommand();
+                dataAdapter.DeleteCommand = bulder.GetDeleteCommand();
+                dataAdapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e) //Добавление строки из полей textBox 
+        {
+            using (MySqlConnection connection = new MySqlConnection(ConnectString))
+            {
+                if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "" || textBox6.Text == "")
+                {
+                    MessageBox.Show("Имеются незаполненные поля!");
+                }
+                else
+                {
+                    try
+                    {
+                        MySqlCommand command = new MySqlCommand("INSERT INTO animals (Помещение,Вид,Кличка,Возраст,Рацион,ФИО)" + "VALUES (@Помещение,@Вид,@Кличка,@Возраст,@Рацион,@ФИО)", connection);
+                        command.Parameters.AddWithValue("@Помещение", textBox1.Text);
+                        command.Parameters.AddWithValue("@Вид", textBox2.Text);
+                        command.Parameters.AddWithValue("@Кличка", textBox3.Text);
+                        command.Parameters.AddWithValue("@Возраст", textBox4.Text);
+                        command.Parameters.AddWithValue("@Рацион", textBox5.Text);
+                        command.Parameters.AddWithValue("@ФИО", textBox6.Text);
+
+                        command.Connection.Open();
+                        command.ExecuteNonQuery();
+                        textBox1.Clear();
+                        textBox2.Clear();
+                        textBox3.Clear();
+                        textBox4.Clear();
+                        textBox5.Clear();
+                        textBox6.Clear();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Возможны следующие ошибки:" +
+                        "Проверьте правильность типов данных!" +
+                        "Проверьте уникальность ключевого поля!");
+                    }
+                }
+            }
+            using (MySqlConnection connection = new MySqlConnection(ConnectString)) //обновление dataGridView 
+            {
+                dataGridView1.DataSource = null;
+                connection.Open();
+                dataAdapter = new MySqlDataAdapter(CommandText, connection);
+                ds = new DataSet();
+                MySqlCommandBuilder bulder = new MySqlCommandBuilder(dataAdapter);
+                dataAdapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+
+
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e) //Удаление выделенной строки 
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.Remove(row);
+            }
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e) //Сохранение данных в БД 
+        {
+
+                using (MySqlConnection connection = new MySqlConnection(ConnectString))
+                {
+                dataAdapter.Update(ds.Tables[0]);
+                }
+            
+        }
+
+        int numberRows = 0;
+
+        private void Button4_Click(object sender, EventArgs e) //Запись в Calc
+        {
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+            ExcelFile ef = new ExcelFile();
+            ExcelWorksheet ws = ef.Worksheets.Add("Writing");
+            MySqlConnection connection = new MySqlConnection(ConnectString);
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(CommandText, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                int i = 0, j;
+                while (reader.Read())
+                {
+                    for (j = 0; j < 6; j++)
+                    {
+                        ws.Cells[i, j].Value = reader[j];
+                    }
+                    i++;
+                }
+                ws.Cells.GetSubrangeAbsolute(0, 0, i - 1, 6).Sort(false).By(0, false).Apply();
+                ef.Save("Zoo.ods");
+                connection.Close();
+                MessageBox.Show("Данные успешно записаны в файл. Файл находится в проекте.");
+                button5.Enabled = true;
+                Process.Start("Zoo.ods");
+
+                numberRows = i;
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e) //Запись в .txt файл
+        {
+            try
+            {
+                SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+                ExcelFile ef = ExcelFile.Load("Zoo.ods");
+                ExcelWorksheet ws = ef.Worksheets[0];
+
+                string getStringOutput, tmp;
+                int j;
+                File.Delete("Zoo.txt");
+
+                StreamWriter writer1 = new StreamWriter("Zoo.txt", true);
+                writer1.WriteLine("+---------------------------------------------------------------------------------------------------+");
+                writer1.WriteLine("|Номер помещения   " + " Вид животного     " + " Кличка        " + " Возраст  " + " Рацион " + "  Фамилия работника зоопарка" + " |\n");
+                writer1.WriteLine("+---------------------------------------------------------------------------------------------------+");
+                writer1.Close();
+
+                for (int i = 0; i < numberRows; i++)
+                {
+                    getStringOutput = "";
+                    for (int k = 0; k < 6; k++)
+                    {
+                        getStringOutput += ws.Cells[i, k].Value.ToString();
+                        tmp = ws.Cells[i, k].Value.ToString();
+
+                        switch (k)
+                        {
+                            case 0:
+                                {
+                                    for (j = 0; j < 15 - tmp.Length; j++)
+                                    {
+                                        getStringOutput += " ";
+                                    }
+                                    break;
+                                }
+
+                            case 1:
+                                {
+                                    for (j = 0; j < 12 - tmp.Length; j++)
+                                    {
+                                        getStringOutput += " ";
+                                    }
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    for (j = 0; j < 10 - tmp.Length; j++)
+                                    {
+                                        getStringOutput += " ";
+                                    }
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    for (j = 0; j < 5 - tmp.Length; j++)
+                                    {
+                                        getStringOutput += " ";
+                                    }
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    for (j = 0; j < 15 - tmp.Length; j++)
+                                    {
+                                        getStringOutput += " ";
+                                    }
+                                    break;
+                                }
+                            case 5:
+                                {
+                                    for (j = 0; j < 12 - tmp.Length; j++)
+                                    {
+                                        getStringOutput += " ";
+                                    }
+                                    break;
+                                }
+                        }
+                        getStringOutput += "\t";
+                    }
+
+                    StreamWriter writer = new StreamWriter("Zoo.txt", true);
+                    writer.WriteLine("|" + getStringOutput + "    |\n");
+                    writer.Close();
+                }
+
+                StreamWriter writer2 = new StreamWriter("Zoo.txt", true);
+                writer2.WriteLine("+---------------------------------------------------------------------------------------------------+");
+                writer2.Close();
+                MessageBox.Show("Данные успешно записаны в файл для печати. Файл находится в проекте. ");
+
+                Process.Start("Zoo.txt");
+                
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+       
+    }
+}
